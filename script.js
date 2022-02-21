3393ec53667a6bcde920c8c05d10db146b2fe92d
cosmin-1409 <cosminnicolaem@alumnos.iesgalileo.es>
2022-02-21 14:40:43 +0100

    Añado objeto de prueba que será el que use para los barcos, drag & drop

--- script.js
+++ script.js
@@ -8,11 +8,40 @@ tablero1.forEach((tr)=>{
 
 
 jugador1.addEventListener('click',(e)=>{
-    e.target.setAttribute('class','barco');
-    if(celdas.includes(e.target)){
-        console.log('boink')
+    if(e.target.nodeName == "TD"){
+        e.target.setAttribute('class','golpe');
     }
 })
 
+let boton = document.getElementById('coger-barco')
 
 
+
+window.onload = function(){
+    draggable('one');
+};
+
+var dragObj = null;
+function draggable(id)
+{
+    var obj = document.getElementById(id);
+    obj.style.position = "absolute";
+    obj.onmousedown = function(){
+            dragObj = obj;
+    }
+}
+
+document.onmouseup = function(e){
+    dragObj = null;
+};
+
+document.onmousemove = function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    if(dragObj == null)
+        return;
+
+    dragObj.style.left = x +"px";
+    dragObj.style.top= y +"px";
+};

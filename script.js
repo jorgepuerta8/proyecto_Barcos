564954baf4251aea49d050dc8a4e7ff83e7bdf66
cosmin-1409 <cosminnicolaem@alumnos.iesgalileo.es>
2022-02-22 14:22:37 +0100

    Cambio en la mecánica

--- script.js
+++ script.js
@@ -1,49 +1,16 @@
-let jugador1 = document.getElementById('Tablero1')
-let tablero1 = [];
-tablero1 = [...jugador1.children[0].children];
-celdas = [];
-tablero1.forEach((tr)=>{
-    celdas.push(...tr.children);
-})
 
+function crearTabla(size){
+    let fragmentoDOM = document.createDocumentFragment();
+    let fila = document.createElement('tr');
+    let nr;
+    let celda;
 
-jugador1.addEventListener('click',(e)=>{
-    if(e.target.nodeName == "TD"){
-        e.target.setAttribute('class','golpe');
+    if(size < 4){
+        size = 4;
     }
-})
+    else if(size > 10){
+        size = 10;
+    }
 
-let boton = document.getElementById('spawn-barco')
-
-boton.addEventListener('click',(e)=>{
-    let celda = e.target;
-    celda = celdas[12].setAttribute('class','golpe');
-    let counter = 12;
-    e.target.addEventListener('keydown',(e)=>{
-        if(e.keyCode ==39){
-            counter ++;
-            celdas[counter-2].removeAttribute('class','golpe');
-            celdas[counter].setAttribute('class','golpe');
-        }
-        if(e.keyCode ==37){
-            counter --;
-            celdas[counter+2].removeAttribute('class','golpe');
-            celdas[counter].setAttribute('class','golpe');
-        }
-        if(e.keyCode ==38){
-            celdas[counter].removeAttribute('class','golpe');
-            celdas[counter-1].removeAttribute('class','golpe');
-            counter -=10;
-            
-            celdas[counter].setAttribute('class','golpe');
-        }
-        if(e.keyCode ==40){
-            celdas[counter].removeAttribute('class','golpe');
-            celdas[counter+1].removeAttribute('class','golpe');
-            counter +=10;
-            
-            celdas[counter].setAttribute('class','golpe');
-        }
-    })
-})
-
+    
+}
